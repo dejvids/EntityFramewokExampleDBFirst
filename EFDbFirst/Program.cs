c87f4f7f23a7eff83a54118e6841e3608146a7b0
dejvid <dawidsurys@hotmail.com>
2017-02-03 15:33:31 +0100

    ended database first

--- EFDbFirst/Program.cs
+++ EFDbFirst/Program.cs
@@ -53,6 +53,68 @@ namespace EFDbFirst
                 foreach (var p in people)
                     Console.WriteLine($"{p.FirstName} {p.LastName} {p.DateOfBirth}");
             }
+            Console.WriteLine("Update:");
+            using (var context = new SucientlyExamplesEntities())
+            {
+                ////Update sandra
+                //Person sandra = context.People.FirstOrDefault(p => p.FirstName == "Sandra");
+                //sandra.FirstName = "John";
+
+                ////create Google ceo
+                //Person sunder = new Person();
+                //sunder.FirstName = "Sundar";
+                //sunder.LastName = "Pichai";
+                //sunder.DateOfBirth = new DateTime(1972, 7, 12);
+                //sunder.Genderld = 1;
+                //context.People.Add(sunder);
+
+                ////remove Steve Jobs
+                ////Person jobs = context.People.FirstOrDefault(p => p.LastName == "Jobs");
+                ////context.People.Remove(jobs);
+                ////context.SaveChanges();
+            }
+
+            using (var context = new SucientlyExamplesEntities())
+            {
+                //using annonymous type
+                var anon = context.People.Where(p => p.LastName == "Jacobs")
+                    .Select(p => new
+                    {
+                        Id = p.id,
+                        FirstName = p.FirstName,
+                        LastName = p.LastName
+                    }).SingleOrDefault();
+                Console.WriteLine($"Annonymous type {anon.FirstName} {anon.LastName}");
+
+                //PersonModel known = context.People.Where(p => p.FirstName == "Emma")
+                //    .Select(p => new Person
+                //    {
+                //        id = p.id,
+                //        FirstName = p.FirstName,
+                //        LastName = p.LastName,
+                //        DateOfBirth = p.DateOfBirth
+                //    }).SingleOrDefault();
+                //Console.WriteLine($"Known type {known.id} {known.FirstName}");
+            }
+
+            using (var context = new SucientlyExamplesEntities())
+            {
+                var sander = context.People.Where(p => p.FirstName == "Sundar")
+                    .Select(p => new { Id = p.id }).SingleOrDefault();
+                //update
+                Person update = new Person();
+                update.id = sander.Id;
+                context.People.Attach(update);
+                update.FirstName = "Tom";
+                //delete
+                var bill = context.People.Where(p => p.FirstName == "Bill")
+                    .Select(p => new { Id = p.id }).SingleOrDefault();
+                Person delete = new Person();
+                delete.id = bill.Id;
+                context.People.Attach(delete);
+                context.People.Remove(delete);
+                context.SaveChanges();
+            }
                 Console.ReadKey();
         }
     }
